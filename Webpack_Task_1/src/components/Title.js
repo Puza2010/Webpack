850374f81376afa4990185b5ede3b48463f426de
Przemek <puza147@gmail.com>
2017-12-19 10:44:29 +0100

    16_4

---
+++ Webpack_Task_1/src/components/Title.js
@@ -0,0 +1,5 @@
+import App from '../containers/App.js';
+
+const Title = props => <h1>{props.title} {counter}</h1>
+
+export default Title;
---
+++ Webpack_Task_1/src/containers/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './App.css';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [],
+            counter: 0
+        };
+    }
+    addTodo(val){
+        const todo = {
+            text: val,
+            id: uuid.v4(),
+        };
+        const data = [...this.state.data, todo];
+        this.setState({data});
+    }
+    removeTodo(id) {
+        const remainder = this.state.data.filter(todo => todo.id !== id);
+        this.setState({data: remainder});
+    }
+
+    countTasks(value){
+        const tasks = {
+            number: value,
+        };
+        const counter = todo.length;
+        this.setState({counter});
+    }
+
+    render() {
+        return (
+            <div className={style.TodoApp}>
+                Tutaj pojawią się komponenty naszej aplikacji.
+            </div>
+        );
+    }
+}
+
+export default App;
